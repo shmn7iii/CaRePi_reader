1131db53897352c2b70d7247f845e31e9c157ef1
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-05-09 15:29:53 +0900

    update: add rootless nfc handling

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 sudo apt-get install -y python3-pip
 sudo pip3 install -r requirements.txt
 sudo modprobe -r port100
+sudo sh -c 'echo SUBSYSTEM==\"usb\", ACTION==\"add\", ATTRS{idVendor}==\"054c\", ATTRS{idProduct}==\"06c3\", GROUP=\"plugdev\" >> /etc/udev/rules.d/nfcdev.rules'
+sudo udevadm control -R
+
+echo -n 're-attach device (y)'
+read str
 
 vendor=054c:06c3
 devices=$(lsusb -d $vendor | sed -e 's|Bus \(...\) Device \(...\).*|usb:\1:\2|')
