176ac669d92c8711d041dcd1f72a892ee1e503c7
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-05-09 16:26:38 +0900

    update: improve sed command

--- setup.sh
+++ setup.sh
@@ -11,5 +11,4 @@ read str
 
 vendor=054c:06c3
 devices=$(lsusb -d $vendor | sed -e 's|Bus \(...\) Device \(...\).*|usb:\1:\2|')
-
-sed -i -e "s/xxx\:xxx\:xxx/$devices/" config.ini
+sed -i -e "/usb_bus_device/c usb_bus_device = $devices" config.ini
